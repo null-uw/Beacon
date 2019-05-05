4979572f62b525362c6ee652011a50e50c010327
Ben Basinski <benbasinski97@gmail.com>
2019-05-04 22:25:45 -0700

    add firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'dart:io' show Platform;
 
 import 'screens/sign_in.dart';
 import 'screens/sign_up.dart';
 import 'screens/home_screen.dart';
 
-void main() => runApp(MyApp());
+// void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+Future<void> main() async {
+  final FirebaseApp app = await FirebaseApp.configure(
+    name: 'db2',
+    options: Platform.isIOS
+        ? const FirebaseOptions(
+            googleAppID: '1:940614893575:ios:37266125a5e1c1b7',
+            databaseURL: 'https://beacon-1177a.firebaseio.com',
+          )
+        : const FirebaseOptions(
+            googleAppID: '1:940614893575:android:37266125a5e1c1b7',
+            databaseURL: 'https://beacon-1177a.firebaseio.com',
+          ),
+  );
+  runApp(MyApp(app: app));
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({this.app});
+  final FirebaseApp app;
+
+  @override
+  _MyState createState() => _MyState();
+}
+
+class _MyState extends State<MyApp> {
   @override
   Widget build(BuildContext ctxt) {
     return new MaterialApp(
--- lib/widgets/search-friends.dart
+++ lib/widgets/search-friends.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class SearchFriends extends StatelessWidget {
+  final databaseReference = FirebaseDatabase.instance.reference();
+
   @override
   Widget build(BuildContext ctx) {
+    databaseReference.once().then((DataSnapshot snapshot) {
+      print('Data : ${snapshot.value}');
+    });
+
     return Text("Add friend");
   }
 }
