243b115d53fdec4079aba8cd684afd8b9a3dab74
Matthew Li <matthewli@Matthews-MacBook-Pro.local>
2019-05-07 18:44:15 -0700

    Add Firebase stream connection logic for user friend locations.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'dart:async';
 
 import 'friend_preferences.dart';
-import '../widgets/map.dart';
+import '../widgets/map-view.dart';
 import '../widgets/friend-list.dart';
 import '../widgets/current-user.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  HomeScreenState createState() => new HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  StreamSubscription _subscriptionFriends;
+  Map<String, StreamSubscription> _locationSubscriptions;
+  Map<String, dynamic> _userLocations;
+
+  @override
+  void initState() {
+    _locationSubscriptions = new Map();
+    _userLocations = new Map();
+
+    FirebaseUserConnector.getUserFriendStream(_onChange)
+        .then((StreamSubscription s) => _subscriptionFriends = s);
+
+    super.initState();
+  }
+
+  // Handles changes to the user's friend node in Firebase Database.
+  // Inner method 'iterateMapEntry' loops through returned snapshot,
+  // instantiating new StreamSubscriptions if not found in state map.
+  _onChange(value) {
+    void iterateMapEntry(key, value) {
+      if (!_locationSubscriptions.containsKey(key)) {
+        FirebaseUserConnector.getUserLocationStream(key, _onLocationChange)
+            .then((StreamSubscription s) => _locationSubscriptions[key] = s);
+      }
+    }
+
+    value.forEach(iterateMapEntry);
+  }
+
+  // Handles changes to a user's location node in Firebase Database.
+  // Updates state variable _userLocations with new value returned from Stream.
+  _onLocationChange(key, value) {
+    _userLocations[key] = value;
+    print(_userLocations);
+  }
+
+  // Destroys all streams when Widget is unmounted.
+  @override
+  void dispose() {
+    if (_subscriptionFriends != null) {
+      _subscriptionFriends.cancel();
+    }
+
+    for (StreamSubscription value in _locationSubscriptions.values) {
+      if (value != null) {
+        value.cancel();
+      }
+    }
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext ctx) {
     _showLogout() {
@@ -54,10 +115,45 @@ class HomeScreen extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Expanded(child: Map()),
+            Expanded(child: MapView()),
             Expanded(child: FriendList()),
             CurrentUser()
           ],
         ));
+  }
+}
+
+class FirebaseUserConnector {
+  static Future<StreamSubscription<Event>> getUserFriendStream(
+      void onData(dynamic)) async {
+    //FirebaseUser currentUser = await FirebaseAuth.instance.currentUser();
+
+    // Creates a StreamSubscription to the current user's friend node in FB database.
+    StreamSubscription<Event> subscription = FirebaseDatabase.instance
+        .reference()
+        .child("users")
+        .child(
+            "uid") // hardcoded to grab uid in FB. Will be changed to currenUser.uid once auth is established
+        .child("friends")
+        .onValue
+        .listen((Event event) {
+      onData(event.snapshot.value);
+    });
+
+    return subscription;
+  }
+
+  // Creates a StreamSubscription to a location node in FB database.
+  static Future<StreamSubscription<Event>> getUserLocationStream(
+      String userKey, void onData(String, dynamic)) async {
+    StreamSubscription<Event> subscription = FirebaseDatabase.instance
+        .reference()
+        .child("locations")
+        .child(userKey)
+        .onValue
+        .listen((Event event) {
+      onData(userKey, event.snapshot.value);
+    });
+    return subscription;
   }
 }
--- lib/widgets/map-view.dart
+++ lib/widgets/map-view.dart
@@ -3,12 +3,12 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class Map extends StatefulWidget {
+class MapView extends StatefulWidget {
   @override
   _MapState createState() => _MapState();
 }
 
-class _MapState extends State<Map> {
+class _MapState extends State<MapView> {
   Completer<GoogleMapController> _controller = Completer();
 
   static const LatLng _center = const LatLng(47.6062, -122.3321);
