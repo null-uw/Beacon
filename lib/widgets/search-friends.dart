d4dff465f3939fd0e7f98adb191d3c3214f404f2
Ben Basinski <benbasinski97@gmail.com>
2019-05-28 17:12:06 -0700

    search for user by email

--- lib/widgets/search-friends.dart
+++ lib/widgets/search-friends.dart
@@ -1,15 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 
-class SearchFriends extends StatelessWidget {
+class SearchFriends extends StatefulWidget {
+  @override
+  _SearchFriendsState createState() => _SearchFriendsState();
+}
+
+class _SearchFriendsState extends State<SearchFriends> {
+  Map result;
   final databaseReference = FirebaseDatabase.instance.reference();
 
   @override
   Widget build(BuildContext ctx) {
-    databaseReference.once().then((DataSnapshot snapshot) {
-      print('Data : ${snapshot.value}');
-    });
+    return new Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text("Add friend",
+              style:
+                  new TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold)),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+            child: new TextField(
+                onSubmitted: searchFriend,
+                autocorrect: false,
+                textCapitalization: TextCapitalization.none,
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.search,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), hintText: "Search by email")),
+          ),
+          result != null ? Text(result['name']) : Text('')
+        ],
+      ),
+    );
+  }
 
-    return Text("Add friend");
+  searchFriend(value) async {
+    print(value);
+
+    DataSnapshot snapshot = await databaseReference
+        .child("index")
+        .orderByChild('email')
+        .equalTo(value)
+        .once();
+
+    var uid = snapshot.value.keys.first;
+
+    setState(() {
+      result = Map.from({
+        "name": snapshot.value[uid]['name'],
+        "email": snapshot.value[uid]['email']
+      });
+    });
   }
 }
